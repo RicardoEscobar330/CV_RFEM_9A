b49b0edde989c3e73eb06b2acf581f94a5a9cb4b
RicardoEscobar330 <20223tn014@utez.edu.mx>
2025-05-26 17:44:25 -0600

    [FIRST COMMIT] Subida inical del proyecto

---
+++ src/main/java/mx/edu/utez/cvrfem9a/Cvrfem9aApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvrfem9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvrfem9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvrfem9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvrfem9a/Cvrfem9aApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvrfem9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvrfem9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
